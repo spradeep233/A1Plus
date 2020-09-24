7b235e786c2fcb3b59d2b54926679400277de1e2
Ashutosh <ashutosh.rai@ondemandtechies.com>
2020-09-25 02:32:21 +0530

    Updates

---
+++ precache-manifest.2cd7e487a836c178107613019c4b1d85.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46dbfada946517fcc14e6935cdeabe3e",
+    "url": "/A1Plus/index.html"
+  },
+  {
+    "revision": "f0a54a0e38c3ed17a7d4",
+    "url": "/A1Plus/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "cca6e6eb0b0110a37af4",
+    "url": "/A1Plus/static/css/main.f787df45.chunk.css"
+  },
+  {
+    "revision": "f0a54a0e38c3ed17a7d4",
+    "url": "/A1Plus/static/js/2.4ca1f866.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/A1Plus/static/js/2.4ca1f866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cca6e6eb0b0110a37af4",
+    "url": "/A1Plus/static/js/main.0802967b.chunk.js"
+  },
+  {
+    "revision": "408e80815183aed86dd5",
+    "url": "/A1Plus/static/js/runtime-main.8d30dc50.js"
+  },
+  {
+    "revision": "a5a56952ac3adaa7147317c5fd0447bf",
+    "url": "/A1Plus/static/media/adolfo-felix-uzvV5U00Cdg-unsplash.a5a56952.jpg"
+  },
+  {
+    "revision": "8032eee6817078e6dacd1f9928bc4ce6",
+    "url": "/A1Plus/static/media/david-becker-YCF34YywBrs-unsplash.8032eee6.jpg"
+  },
+  {
+    "revision": "3dce31ee44ce051b64d360d61db564f8",
+    "url": "/A1Plus/static/media/james-fitzgerald-V93zr0pCou0-unsplash.3dce31ee.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/A1Plus/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1e419f30fdc663f02bc540f1fcfcf3a4",
+    "url": "/A1Plus/static/media/lucas-sankey-WM6Rj6yITvs-unsplash.1e419f30.jpg"
+  },
+  {
+    "revision": "606637fab077583319c3e359308ca070",
+    "url": "/A1Plus/static/media/paul-postema-wmDiqxxuKJ8-unsplash.606637fa.jpg"
+  },
+  {
+    "revision": "5d04d3f9ac0b27134a929af7e638cd96",
+    "url": "/A1Plus/static/media/rishi-W55xcOxI-Ig-unsplash.5d04d3f9.jpg"
+  },
+  {
+    "revision": "18083377c4b451a9c0ecdcc635b0710e",
+    "url": "/A1Plus/static/media/sandra-seitamaa-7-3cGWyQlV0-unsplash.18083377.jpg"
+  },
+  {
+    "revision": "38a222e9059bd4f75339d95c106a1c00",
+    "url": "/A1Plus/static/media/shashi-prakash-gautam-K5M1L2MdJTw-unsplash.38a222e9.jpg"
+  }
+]);
